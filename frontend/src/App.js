2a451e92768aff824c6d0925849cfab4901d7b2f
github username <github email address>
2021-08-26 18:52:45 -0400

    add nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,83 @@
 import './App.css';
+import {Route, NavLink} from 'react-router-dom'
+import './index.css';
+
+const watch = () => {
+  return(
+   <div className="watch">
+     <NavLink
+     to="/watch"
+     exact
+     activeStyle={{
+       background: "darkblue",
+     }}
+      >
+    <h1>watch</h1></NavLink>
+    </div>
+  )
+}
+
+const home = () => {
+  return(
+    <div>
+      <NavLink
+      to="/"
+      exact
+      activeStyle={{
+        background:"darkblue",
+      }}
+      >
+      <h1>home</h1></NavLink>
+     
+      <NavLink
+      to="/watch"
+      exact
+      activeStyle={{
+        background:"darkblue",
+      }}
+      >
+      <h1>watch</h1></NavLink>
+      
+      <NavLink
+      to="/ask"
+      exact
+      activeStyle={{
+        background:"darkblue",
+      }}
+      >
+      <h1>ask</h1></NavLink>
+
+    </div>
+     
+  )
+}
+
+const ask = () => {
+  return (
+    <div>
+     <NavLink 
+     to="/"
+     exact
+     activeStyle={{
+      background: "darkblue",
+    }}
+    >
+     <h1>cool!</h1></NavLink>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Route exact path="/" component={home}/>
+     <Route exact path="/ask" component={ask}/>
+     <Route exact path="/watch" component={watch}/>
     </div>
   );
 }
 
 export default App;
+
+
