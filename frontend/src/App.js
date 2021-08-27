aab4c2541a583111a34e19f892c6403376dff235
github username <github email address>
2021-08-26 23:30:33 -0400

    adds background style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Route, NavLink} from 'react-router-dom'
 import './index.css';
 
+
 const watch = () => {
   return(
    <div className="watch">
@@ -20,29 +21,32 @@ const watch = () => {
 const home = () => {
   return(
     <div>
-      <NavLink
+      <NavLink 
+      id = "home"
       to="/"
       exact
       activeStyle={{
-        background:"darkblue",
+        color:"pink",
       }}
       >
       <h1>home</h1></NavLink>
      
       <NavLink
+      id="watch"
       to="/watch"
       exact
       activeStyle={{
-        background:"darkblue",
+        color:"pink",
       }}
       >
       <h1>watch</h1></NavLink>
       
       <NavLink
+      id="ask"
       to="/ask"
       exact
       activeStyle={{
-        background:"darkblue",
+        background:"",
       }}
       >
       <h1>ask</h1></NavLink>
@@ -68,12 +72,15 @@ const ask = () => {
 }
 
 
+
 function App() {
   return (
     <div className="App">
+      <div className="Header">
       <Route exact path="/" component={home}/>
      <Route exact path="/ask" component={ask}/>
      <Route exact path="/watch" component={watch}/>
+      </div>
     </div>
   );
 }
